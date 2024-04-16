af7d482d8fdacb68376d4637239161c697270383
brian9work <brian9work@gmail.com>
2024-04-16 14:01:32 -0600

    previo a comentar

--- tareas/examen/sincrono.js
+++ tareas/examen/sincrono.js
@@ -1,29 +1,17 @@
+// Creamos un arreglo con todos nuestros caracteres
 const array = ['?','°','7','¿','P','~','j','Y','B','U','Á','"','L','*','-','u','¡','x','I','M','A','q','Q','|','”','b','V','T','C','8',')','1','m','v',',','9','D','¨','p','6',' ','c','y','#','e','ú','r','h','Ú','a','R','Í','.','O','3','2','ñ','Ñ','w','@','g','F','&','“','W','!','$','n','d','E','%','t','(','Ó','0','ó','s','_','z','í','o',';','H','=','f','i','S','l','á',':','4','+','G','k','X','K','É','5','N','é','J','Z'
 ];
-function aleatorizarArray(array) {
-    // Recorremos el array desde el final hacia el principio
-    for (var i = array.length - 1; i > 0; i--) {
-        // Generamos un índice aleatorio entre 0 e i
-        var j = Math.floor(Math.random() * (i + 1));
-        // Intercambiamos los elementos en las posiciones i y j
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
-}
-const advance = (key)=>{
+
+// Con esra funcion no pasaran como parametro la llave en forma de arreglo
+const avanzarCaracteres = (key)=>{
+    // La recorreremos con la funcion map y obtendremos el valor de la posicion donde se encuentra 
     return (key.map(
         w => (array.indexOf(w))
+        // Posteriormente sumaremos todos los valores usando la funcion reduce y le agregaremos uno en caso de que la posicion del caracter sea 0
     ).reduce((a, b) => a + b, 0))+1
 }
 const encriptar=(mensaje,key)=>{
-    // console.log(mensaje)
-    // console.log(key)
-
-    console.log(advance(key.split("")))
-
-    for (let i=2; i<=advance(key.split("")); i++) {
+    for (let i=2; i<=avanzarCaracteres(key.split("")); i++) {
         var expresionRegular = new RegExp(".{1," + i + "}", "g");
         mensaje =
         mensaje.match(expresionRegular).map(b =>{
@@ -31,18 +19,12 @@ const encriptar=(mensaje,key)=>{
                 b.split("").reverse().join("")
             )
         }).join("")
-        // console.log(mensaje)
     }
 
     return mensaje
 }
 const desencriptar=(mensaje,key)=>{
-    // console.log(mensaje)
-    // console.log(key)
-
-    console.log(advance(key.split("")))
-
-    for (let i=advance(key.split("")); i>=2; i--) {
+    for (let i=avanzarCaracteres(key.split("")); i>=2; i--) {
         var expresionRegular = new RegExp(".{1," + i + "}", "g");
         mensaje =
         mensaje.match(expresionRegular).map(b =>{
@@ -50,7 +32,6 @@ const desencriptar=(mensaje,key)=>{
                 b.split("").reverse().join("")
             )
         }).join("")
-        // console.log(mensaje)
     }
 
     return mensaje
@@ -72,6 +53,5 @@ document.getElementById("desencriptar").addEventListener("click",e =>{
     document.getElementById("respuesta").value=desencriptar(mensaje,llave)
 })
 
-// const publicKey = "HEGB030312HTLRRRA7";
-// const privateKey = "t_t";
+// const Key = "HEGB030312HTLRRRA7";
 
